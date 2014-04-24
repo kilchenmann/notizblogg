8e4148d9c0863d49f0dffd43e72056b3ca41318b
André Kilchenmann <dhlab@milchkannen.ch>
2014-04-24 23:54:03 +0200

    searchpanel as jquery.finder plugin, less template for this project (notizblogg)

---
+++ core/bin/js/jquery.finder.js
@@ -0,0 +1,101 @@
+/* ===========================================================================
+ *
+ * @frame: jQuery plugin for lakto — flat one page and responsive webdesign template
+ *
+ * @author André Kilchenmann code@milchkannen.ch
+ *
+ * @copyright 2014 by André Kilchenmann (milchkannen.ch)
+ *
+ * @requires
+ *  jQuery - min-version 1.10.2
+ *
+ * ===========================================================================
+ * ======================================================================== */
+
+(function( $ ){
+	// -----------------------------------------------------------------------
+	// define some functions
+	// -----------------------------------------------------------------------
+	//
+	// -------------------------------------------------------------------------
+	// define the methods
+	// -------------------------------------------------------------------------
+
+	var methods = {
+		/*========================================================================*/
+		init: function(options) {
+			return this.each(function() {
+				var $this = $(this),
+					localdata = {};
+
+
+				localdata.search = {};
+
+				localdata.settings = {
+					placeholder: 'search',
+					filter: 'search filter',
+					database: ''
+				};
+				$.extend(localdata.settings, options);
+				// initialize a local data object which is attached to the DOM object
+				$this.data('localdata', localdata);
+
+				// set the title of the document and the project specific data
+				$this
+					.append(localdata.search.filter = $('<input>')
+						.attr({
+							'type': 'button',
+							'title': localdata.settings.filter
+						})
+						.addClass('btn grp_left search_filter')
+					)
+					.append(localdata.search.field = $('<input>')
+						.attr({
+							'type': 'search',
+							'title': localdata.settings.placeholder,
+							'placeholder': localdata.settings.placeholder
+						})
+						.addClass('input grp_middle search_field')
+					)
+					.append(localdata.search.button = $('<input>')
+						.attr({
+							'type': 'button',
+							'title': localdata.settings.placeholder
+						})
+						.addClass('btn grp_right search_btn')
+					);
+				$this.append(
+					localdata.search.extended = $('<div>')
+						.addClass('float_obj search_extended')
+						.html('Hier ein Formular wie bisher')
+				);
+
+				localdata.search.filter.on('click', function(event) {
+					localdata.search.extended.toggle();
+				});
+
+			});											// end "return this.each"
+		},												// end "init"
+
+		anotherMethod: function() {
+			return this.each(function(){
+				var $this = $(this);
+				var localdata = $this.data('localdata');
+			});
+		}
+		/*========================================================================*/
+	};
+
+
+
+	$.fn.finder = function(method) {
+		// Method calling logic
+		if ( methods[method] ) {
+			return methods[ method ].apply( this, Array.prototype.slice.call( arguments, 1 ));
+		} else if ( typeof method === 'object' || ! method ) {
+			return methods.init.apply( this, arguments );
+		} else {
+			throw 'Method ' + method + ' does not exist on jQuery.tooltip';
+		}
+	};
+})( jQuery );
