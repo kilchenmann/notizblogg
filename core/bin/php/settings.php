9ec962ffb8f450f704bbeebefbce83cb448a62b0
André Kilchenmann <gaston@milchkannen.ch>
2014-09-24 00:31:53 +0200

    default preferences

--- core/bin/php/settings.php
+++ core/bin/php/settings.php
@@ -22,7 +22,7 @@ define ("__MEDIA_URL__", "http://localhost/media");		// http://somewhere.com/MED
 define ('__MEDIA_PATH__', "/Library/WebServer/Documents/media");									// /Library/WebServer/Documents/nb
 define ('__DOWNLOAD_URL__', $protocol.$_SERVER['HTTP_HOST'] . "/data/export");	// http://somewhere.com/EXPORT
 // echo "DOWNLOAD_URL: ".__DOWNLOAD_URL__."<br>";
-define ('__SITE_API__', $protocol.$_SERVER['HTTP_HOST'] . "/" . __BASE_FOLDER__ . "/api");			// http://somewhereelse.com/API
+define ('__SITE_API__', $protocol.$_SERVER['HTTP_HOST'] . __BASE_FOLDER__ . "/api");			// http://somewhereelse.com/API
 // echo "SITE_API: ".__SITE_API__."<br>";
 
 include ('functions.php');
